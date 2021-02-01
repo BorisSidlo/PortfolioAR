9eb75b73399a63a7a49050f2a91e9567cac785ce
BorisSidlo <boris.sidlo@icloud.com>
2021-02-01 02:05:46 +0100

    ViewController: UIViewController done

--- PortfolioAR/ViewController.swift
+++ PortfolioAR/ViewController.swift
@@ -7,18 +7,81 @@
 
 import UIKit
 import RealityKit
+import ARKit
+//import AR kit
+//delte stfuuf
+
+
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // overvider deafult ar kit
+        super.viewDidAppear(animated)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        setupARview()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
     }
+    
+    func setupARview(){
+        
+        // MARK
+        
+    arView.automaticallyConfigureSession = false
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        
+        // better textures
+        configuration.environmentTexturing = .automatic
+        
+        // run confugration
+        arView.session.run(configuration)
+    }
+
+
+    @objc
+    func handleTap(recognizer: UITapGestureRecognizer) {
+        let location = recognizer.location(in: arView)
+        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+            
+        if let firstResults = results.first{
+            // order to place wee need anchor
+            // multiUser to sycnhornize acnhor need
+            let anchor = ARAnchor(name: "ContemporaryFan", transform: firstResults.worldTransform)
+            arView.session.add(anchor: anchor)
+        }else{
+            print("FIND PLANE ")
+        }
+        
+        
+        }
+    
+   // extension ViewController: ARSessionDelegate{
+       // func session(_session:ARSession, didAdd anchors: [ARAnchor]){
+      //      if let anchor =
+     //   }
+  //  }
+
+
+
+
+
+
+    // tap gesture recognizitaon
+// layser point to virtual laser if it is intesction
+
+        
+// gesture recongize
+
+
+    
+    
+
+    
+    
+  
 }
